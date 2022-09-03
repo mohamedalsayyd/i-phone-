2f4a86c19da78ca053e4a45757aeec3ecedd8272
Lime <78107134+mohamedalsayyd@users.noreply.github.com>
2022-09-03 03:58:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img");
+let bg = document.querySelector(".container");
+
+function changePhone(imgSource) {
+  img.src = imgSource;
+}
+
+function changeBgColor(color) {
+  bg.style.background = color;
+}
